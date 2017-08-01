a80ac59858830e591ce4bb0854744f0a64f42c68
Sam Fortin <sam.j.fortin@gmail.com>
2017-08-01 16:40:36 -0500

    /random route sending a random number

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var app = express();
 
 var port = 5000;
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}
+
+var randomGeneratedNumber = randomNumber(1, 100).toString();
+
 app.use(express.static('public'));
 
-app.listen(port, function(){
+app.get('/random', function (req, res) {
+    res.send(randomGeneratedNumber);
+});
+
+app.listen(port, function () {
     console.log('Listening on port number', port);
+    console.log(randomGeneratedNumber);
 });
